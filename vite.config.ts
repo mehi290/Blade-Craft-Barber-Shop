1d1a9424611fa546a013727caa9fd04c6b84c3cc
mehi290 <luxeutopia6@gmail.com>
2026-05-14 19:24:29 +0400

    fi

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   tanstackStart: {
-    server: { entry: "server" },
-    deploymentTarget: "cloudflare-pages",
+    server: {
+      entry: "src/server.ts" // Ensure this matches your file path
+    },
+    deploymentTarget: "vercel",
   },
 });
